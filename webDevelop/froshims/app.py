c6b978664fae3ce304b95c2cbf4f064c3bad62ec
titoDevelopFromWork <tito.solis@lmco.com>
2023-09-29 16:40:53 +0900

    additional website example work

--- webDevelop/froshims/app.py
+++ webDevelop/froshims/app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template, request
+#Implements a registration form, storing registrants in a SQL db.
+
+
+###-----------Dependencies----------###
+
+from flask import Flask, render_template, request #need to import flask module.
+from tbd import SQL #need to import SQL module.
+
+###-----------Global Declarations----------###
 
 app = Flask (__name__)
 
-REGISTRANTS = {} #Declares an empty dictionary. Also, all caps because you cannot have a variable and a function have the same name.
-#coding convention is to use all caps for global variables.
+db = SQL("") #enter the path to the SQL db
 
 SPORTS = [
     "Basketball",
@@ -11,21 +18,40 @@ SPORTS = [
     "Ultimate Frisbee"
 ]
 
+###-----------Juicy Part of the Web App----------###
+
+#Default Landing Page Logic
 @app.route("/")
 def index():
     return render_template("index.html", sports=SPORTS)
 
+
+#Register Registrant Logic 
 @app.route("/register", methods=["POST"])
 def register():
+    #Validate submission
     name = request.form.get("name")
-    if not name:
-        return render_template("failure.html")
     sport = request.form.get("sport")
-    if sport not in SPORTS:
+    if not name or sport in SPORTS:
         return render_template("failure.html")
-    REGISTRANTS[name] = sport #enters a sport value for the name key (key:value pair)
-    return render_template("success.html")
+    
+    #Remember registrant
+    db.execute("INSERT INTO registrants (name, sport) VALUES(?, ?)", name, sport)
 
+    #Confirm registration
+    return redirect("/registrants")
+
+#Return Registrant List Logic
 @app.route("/registrants")
 def registrants():
-    return render_template("registrants.html", registrants=REGISTRANTS)
+    registrants = db.execute("SELECT * FROM registrants")
+    return render_template("registrants.html", registrants=registrants)
+
+#Deregister Registrant Logic
+@app.route("/deregister", methods=["POST"])
+def deregister():
+    #Forget registrant
+    id = request.form.get("id")
+    if id:
+        db.execute("DELETE FROM registrants WHERE id =?, id") #Need to implement security measures so only those with permisions can delete db items. Look into Flask's session variable
+    return redirect("/registrants")
