4e7b97340c2674ccc2f2ad095c0aa7a0897d0fd4
sorisu <titto.develop@gmail.com>
2023-11-10 17:37:30 -0700

    creating sample python classes

---
+++ CS502022/main.py
@@ -0,0 +1,16 @@
+#-----Dependecies-----#
+
+
+#-----Definitions-----#
+class Item:
+    pass
+
+#-----Instantces-----#
+
+item1 = Item()
+item1.name = "Phone"
+item1.price = 100
+item1.quantity = 5
+
+print(type(item1))
+print(type(item1.name))
